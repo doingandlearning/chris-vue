3f0d2d04d0e904712c5c5cc34285e7e23534d168
Kevin Cunningham <kevin@kevincunningham.co.uk>
2023-08-02 16:19:09 +0100

    Added sommit

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import TheWelcome from './components/TheWelcome.vue'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
+      <p>Make a change</p>
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
